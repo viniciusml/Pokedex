09144304bfcc0065c17b4fefe14c3daa3675dae9
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-05-28 23:31:08 +0200

    Navigation controller router presents Controller

---
+++ PokedexTests/NavigationControllerRouterTests.swift
@@ -0,0 +1,38 @@
+//
+//  NavigationControllerRouterTests.swift
+//  PokedexTests
+//
+//  Created by Vinicius Moreira Leal on 28/05/2020.
+//  Copyright © 2020 Vinicius Moreira Leal. All rights reserved.
+//
+
+import XCTest
+
+public protocol Router {
+    func routeTo(pokemon urlString: String)
+}
+
+class NavigationControllerRouter: Router {
+
+    let navigationController: UINavigationController
+
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func routeTo(pokemon urlString: String) {
+        navigationController.pushViewController(UIViewController(), animated: false)
+    }
+}
+
+class NavigationControllerRouterTests {
+
+    func test_routToPokemon_presentPokemonViewController() {
+        let navigationController = UINavigationController()
+        let sut = NavigationControllerRouter(navigationController)
+
+        sut.routeTo(pokemon: "http://pokemon1.com")
+
+        XCTAssertEqual(navigationController.viewControllers.count, 1)
+    }
+}
