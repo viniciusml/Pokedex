f284c1da462193390a52e1a380240b61004357f0
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-12-10 21:29:07 +0000

    Add 'PokedexSnapshotTests' target

---
+++ PokedexSnapshotTests/PokedexSnapshotTests.swift
@@ -0,0 +1,15 @@
+//
+//  PokedexSnapshotTests.swift
+//  PokedexSnapshotTests
+//
+//  Created by Vinicius Moreira Leal on 10/12/2020.
+//  Copyright © 2020 Vinicius Moreira Leal. All rights reserved.
+//
+
+import XCTest
+
+class PokedexSnapshotTests: XCTestCase {
+
+    func test() {
+    }
+}
