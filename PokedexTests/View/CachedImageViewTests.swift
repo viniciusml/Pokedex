800b5a0a87f6f43ec471045a6c33f4c061d64208
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-12-30 22:14:42 +0000

    Move 'CachedImageView' to specific folder

---
+++ PokedexTests/View/CachedImageViewTests.swift
@@ -0,0 +1,9 @@
+//
+//  CachedImageViewTests.swift
+//  PokedexTests
+//
+//  Created by Vinicius Moreira Leal on 30/12/2020.
+//  Copyright © 2020 Vinicius Moreira Leal. All rights reserved.
+//
+
+import Foundation
