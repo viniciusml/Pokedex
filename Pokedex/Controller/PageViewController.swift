80b19a45309b2637fe520c249bffaae2478fa2a6
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-12-30 21:19:55 +0000

    Add `PageViewController`

---
+++ Pokedex/Controller/PageViewController.swift
@@ -0,0 +1,65 @@
+//
+//  PageViewController.swift
+//  Pokedex
+//
+//  Created by Vinicius Moreira Leal on 30/12/2020.
+//  Copyright © 2020 Vinicius Moreira Leal. All rights reserved.
+//
+
+import UIKit
+
+public class PageViewController: UIPageViewController {
+    
+    private var pages: [UIViewController] = []
+    private var pageControlIndex = 0
+    
+    public convenience init(pages: [UIViewController]) {
+        self.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        self.pages = pages
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstPage = pages.first {
+            setViewControllers([firstPage], direction: .forward, animated: true)
+        }
+    }
+}
+
+extension PageViewController: UIPageViewControllerDataSource {
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else { return nil }
+        
+        return pages[previousIndex]
+    }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < pages.count else { return nil }
+        
+        return pages[nextIndex]
+    }
+    
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+    
+    public func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return pageControlIndex
+    }
+}
---
+++ PokedexTests/PageViewControllerTests.swift
@@ -0,0 +1,139 @@
+//
+//  PageViewControllerTests.swift
+//  PokedexTests
+//
+//  Created by Vinicius Moreira Leal on 30/12/2020.
+//  Copyright © 2020 Vinicius Moreira Leal. All rights reserved.
+//
+
+import XCTest
+import Pokedex
+
+class PageViewControllerTests: XCTestCase {
+    
+    func test_init_hasZeroPages() {
+        XCTAssertTrue(makeSUT().viewControllers!.isEmpty)
+    }
+    
+    func test_init_setsPreferredTransitionStyle() {
+        XCTAssertEqual(makeSUT().transitionStyle, .scroll)
+    }
+    
+    func test_init_setsPreferredNavigationOrientation() {
+        XCTAssertEqual(makeSUT().navigationOrientation, .horizontal)
+    }
+    
+    func test_viewDidLoad_withMoreThanZeroPages_setsInitialViewController() {
+        let pages = [UIViewController()]
+        let sut = makeSUT(pages: pages)
+        
+        XCTAssertEqual(sut.viewControllers, pages)
+    }
+    
+    func test_viewControllerAfter_selectsNextIndex() {
+        let controller1 = UIViewController()
+        let controller2 = UIViewController()
+        let pages = [controller1, controller2]
+        
+        let sut = makeSUT(pages: pages)
+        let controllerAfter = sut.controllerAfter(controller1)
+        
+        XCTAssertEqual(controllerAfter, controller2)
+    }
+    
+    func test_viewControllerAfterTwice_selectsSecondNextIndex() {
+        let controller1 = UIViewController()
+        let controller2 = UIViewController()
+        let controller3 = UIViewController()
+        let pages = [controller1, controller2, controller3]
+        
+        let sut = makeSUT(pages: pages)
+        let controllerAfter = sut.controllerAfter(controller1)
+        let secondControllerAfter = sut.controllerAfter(controllerAfter!)
+        
+        XCTAssertEqual(secondControllerAfter, controller3)
+    }
+    
+    func test_viewControllerAfter_whenMaxNumberOfPagesReached_doesNotSelectNextController() {
+        let controller1 = UIViewController()
+        let pages = [controller1]
+        
+        let sut = makeSUT(pages: pages)
+        let controllerAfter: UIViewController? = sut.controllerAfter(controller1)
+        
+        XCTAssertNil(controllerAfter)
+    }
+    
+    func test_viewControllerBefore_selectsPreviousIndex() {
+        let controller1 = UIViewController()
+        let controller2 = UIViewController()
+        let pages = [controller1, controller2]
+        
+        let sut = makeSUT(pages: pages)
+        let controllerBefore = sut.controllerBefore(controller2)
+        
+        XCTAssertEqual(controllerBefore, controller1)
+    }
+    
+    func test_viewControllerBeforeTwice_selectsSecondPreviousIndex() {
+        let controller1 = UIViewController()
+        let controller2 = UIViewController()
+        let controller3 = UIViewController()
+        let pages = [controller1, controller2, controller3]
+        
+        let sut = makeSUT(pages: pages)
+        let controllerBefore = sut.controllerBefore(controller3)
+        let secondControllerBefore = sut.controllerBefore(controllerBefore!)
+        
+        XCTAssertEqual(secondControllerBefore, controller1)
+    }
+    
+    func test_viewControllerBefore_whenInFirstPage_doesNotSelectPreviousController() {
+        let controller1 = UIViewController()
+        let pages = [controller1]
+        
+        let sut = makeSUT(pages: pages)
+        let controllerBefore: UIViewController? = sut.controllerBefore(controller1)
+        
+        XCTAssertNil(controllerBefore)
+    }
+    
+    func test_presentationCount_setsCorrectNumberOfPages() {
+        let controller1 = UIViewController()
+        let pages = [controller1]
+        
+        let sut = makeSUT(pages: pages)
+        let presentationCount = sut.dataSource?.presentationCount?(for: sut)
+        
+        XCTAssertEqual(presentationCount, pages.count)
+    }
+    
+    func test_presentationIndex_initiatesAtFirstIndex() {
+        let controller1 = UIViewController()
+        let pages = [controller1]
+        
+        let sut = makeSUT(pages: pages)
+        let presentationIndex = sut.dataSource?.presentationIndex!(for: sut)
+        
+        XCTAssertEqual(presentationIndex, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(pages: [UIViewController] = []) -> PageViewController {
+        let sut = PageViewController(pages: pages)
+        sut.loadViewIfNeeded()
+        return sut
+    }
+}
+
+fileprivate extension UIPageViewController {
+    
+    func controllerAfter(_ controller: UIViewController) -> UIViewController? {
+        return dataSource?.pageViewController(self, viewControllerAfter: controller)
+    }
+    
+    func controllerBefore(_ controller: UIViewController) -> UIViewController? {
+        return dataSource?.pageViewController(self, viewControllerBefore: controller)
+    }
+}
