df16778cf60c325de01e498e5b8191923eff4b90
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2021-01-31 18:38:51 +0000

    Move helper method to `SharedHelpers`

--- PokedexTests/Controller/PokemonViewControllerTests.swift
+++ PokedexTests/Controller/PokemonViewControllerTests.swift
@@ -89,40 +89,6 @@ class PokemonViewControllerTests: XCTestCase {
         return (sut, loader, imageLoader)
     }
     
-    private func makeItem(id: Int) -> PokemonItem {
-        PokemonItem(id: id,
-                    name: "bulbasaur",
-                    baseExperience: 64,
-                    height: 7,
-                    isDefault: true,
-                    order: 1,
-                    weight: 69,
-                    abilities: [
-                        Ability(isHidden: true,
-                                slot: 3,
-                                ability: Species(name: "chlorophyll",
-                                                 url: "https://pokeapi.co/api/v2/ability/34/"))],
-                    forms: [Species(name: "bulbasaur",
-                                    url: "https://pokeapi.co/api/v2/pokemon-form/1/")],
-                    species: Species(name: "bulbasaur",
-                                     url: "https://pokeapi.co/api/v2/pokemon-species/1/"),
-                    sprites: Sprites(backFemale: nil,
-                                     backShinyFemale: nil,
-                                     backDefault: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
-                                     frontFemale: nil,
-                                     frontShinyFemale: nil,
-                                     backShiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
-                                     frontDefault: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-                                     frontShiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png"),
-                    stats: [Stat(baseStat: 45,
-                                 effort: 0,
-                                 stat: Species(name: "speed",
-                                               url: "https://pokeapi.co/api/v2/stat/6/"))],
-                    types: [TypeElement(slot: 2,
-                                        type: Species(name: "poison",
-                                                      url: "https://pokeapi.co/api/v2/type/4/"))])
-    }
-    
     private func assertThat(_ sut: PokemonViewController, hasViewConfiguredFor item: PokemonItem?, file: StaticString = #filePath, line: UInt = #line) {
         
         XCTAssertEqual(sut.pokemonName, item?.formattedName, "Expected name text to be \(String(describing: item?.name)) for label", file: file, line: line)
--- PokedexTests/Helpers/SharedTestHelpers.swift
+++ PokedexTests/Helpers/SharedTestHelpers.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import PokemonDomain
 
 func anyData() -> Data {
     Data("any data".utf8)
@@ -22,4 +23,42 @@ func anyNSError() -> NSError {
 
 func makeItemsJSON(_ items: [String: Any]) -> Data {
     try! JSONSerialization.data(withJSONObject: items)
+}
+
+func makeList(count: Int, next: String = "http://pokemon-url.com", results: [ResultItem] = []) -> ListItem {
+    ListItem(count: 3, next: "http://pokemon-url.com", previous: nil, results: results)
+}
+
+func makeItem(id: Int) -> PokemonItem {
+    PokemonItem(id: id,
+                name: "bulbasaur",
+                baseExperience: 64,
+                height: 7,
+                isDefault: true,
+                order: 1,
+                weight: 69,
+                abilities: [
+                    Ability(isHidden: true,
+                            slot: 3,
+                            ability: Species(name: "chlorophyll",
+                                             url: "https://pokeapi.co/api/v2/ability/34/"))],
+                forms: [Species(name: "bulbasaur",
+                                url: "https://pokeapi.co/api/v2/pokemon-form/1/")],
+                species: Species(name: "bulbasaur",
+                                 url: "https://pokeapi.co/api/v2/pokemon-species/1/"),
+                sprites: Sprites(backFemale: nil,
+                                 backShinyFemale: nil,
+                                 backDefault: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+                                 frontFemale: nil,
+                                 frontShinyFemale: nil,
+                                 backShiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+                                 frontDefault: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+                                 frontShiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png"),
+                stats: [Stat(baseStat: 45,
+                             effort: 0,
+                             stat: Species(name: "speed",
+                                           url: "https://pokeapi.co/api/v2/stat/6/"))],
+                types: [TypeElement(slot: 2,
+                                    type: Species(name: "poison",
+                                                  url: "https://pokeapi.co/api/v2/type/4/"))])
 }
