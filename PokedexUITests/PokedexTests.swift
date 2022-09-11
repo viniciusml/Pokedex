ab64938bf2c08167afe7f04fcdcb7b2341fcc48c
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2022-09-11 13:23:25 +0100

    Add first test running in 0.196 (0.198) seconds

--- PokedexUITests/PokedexTests.swift
+++ PokedexUITests/PokedexTests.swift
@@ -9,4 +9,20 @@
 import XCTest
 
 final class PokedexTests: XCTestCase {
+
+    private let app = XCUIApplication()
+
+    func testPokedex() {
+        let pokedexScreen = PokedexScreen(app)
+
+        XCTAssertTrue(pokedexScreen.title.displayed)
+    }
+}
+
+extension XCUIElement {
+
+    var displayed: Bool {
+        guard exists, !frame.isEmpty else { return false }
+        return exists && XCUIApplication().windows.element(boundBy: 0).frame.contains(frame)
+    }
 }
---
+++ PokedexUITests/Screens/PokedexScreen.swift
@@ -0,0 +1,21 @@
+//
+//  PokedexScreen.swift
+//  PokedexUITests
+//
+//  Created by Vinicius Moreira Leal on 11/09/2022.
+//  Copyright © 2022 Vinicius Moreira Leal. All rights reserved.
+//
+
+import XCTest
+
+struct PokedexScreen {
+    private let app: XCUIApplication
+
+    init(_ app: XCUIApplication) {
+        self.app = app
+    }
+
+    var title: XCUIElement {
+        app.staticTexts["Pokédex"]
+    }
+}
