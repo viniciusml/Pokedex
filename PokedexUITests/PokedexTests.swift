4df21a748bfc78f2b2942ffacffa73ed0ee52987
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2022-09-11 13:04:59 +0100

    Create UI test target

---
+++ PokedexUITests/PokedexTests.swift
@@ -0,0 +1,12 @@
+//
+//  PokedexTests.swift
+//  PokedexUITests
+//
+//  Created by Vinicius Moreira Leal on 11/09/2022.
+//  Copyright © 2022 Vinicius Moreira Leal. All rights reserved.
+//
+
+import XCTest
+
+final class PokedexTests: XCTestCase {
+}
